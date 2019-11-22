447504e6d160f0cc0390428a98c1e38a4e0c2c3d
MinexAutomation <minex.automation@gmail.com>
2019-11-22 15:39:01 -0800

    Added text item and text item type identities.

---
+++ source/R5T.Siscia/Code/Classes/TextItemIdentity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Guide;
+
+
+namespace R5T.Siscia
+{
+    public class TextItemIdentity : TypedGuid
+    {
+        #region Static
+
+        public static TextItemIdentity New()
+        {
+            var textItemIdentity = new TextItemIdentity();
+            return textItemIdentity;
+        }
+
+        public static TextItemIdentity From(Guid guid)
+        {
+            var textItemIdentity = new TextItemIdentity(guid);
+            return textItemIdentity;
+        }
+
+        #endregion
+
+
+        public TextItemIdentity(Guid value)
+            : base(value)
+        {
+        }
+
+        public TextItemIdentity()
+            : this(TypedGuid.GetNewGuid())
+        {
+        }
+    }
+}
---
+++ source/R5T.Siscia/Code/Classes/TextItemTypeIdentity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Guide;
+
+
+namespace R5T.Siscia
+{
+    public class TextItemTypeIdentity : TypedGuid
+    {
+        #region Static
+
+        public static TextItemTypeIdentity New()
+        {
+            var textItemTypeIdentity = new TextItemTypeIdentity();
+            return textItemTypeIdentity;
+        }
+
+        public static TextItemTypeIdentity From(Guid guid)
+        {
+            var textItemTypeIdentity = new TextItemTypeIdentity(guid);
+            return textItemTypeIdentity;
+        }
+
+        #endregion
+
+
+        public TextItemTypeIdentity(Guid value)
+            : base(value)
+        {
+        }
+
+        public TextItemTypeIdentity()
+            : this(TypedGuid.GetNewGuid())
+        {
+        }
+    }
+}
