90405cf9c94fe568c7e21cd12c3b6b8d24753842
MinexAutomation <minex.automation@gmail.com>
2019-12-04 12:21:48 -0800

    Added TextItem and TextItemType types.

---
+++ source/R5T.Siscia/Code/Classes/TextItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Siscia
+{
+    public class TextItem
+    {
+        public TextItemIdentity Identity { get; set; }
+        public TextItemType Type { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ source/R5T.Siscia/Code/Classes/TextItemType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Siscia
+{
+    public class TextItemType
+    {
+        public TextItemTypeIdentity Identity { get; set; }
+        public string Name { get; set; }
+    }
+}
